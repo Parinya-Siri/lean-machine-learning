d2dfc650fa185fefae07cd8d5cf4957abbb4bfb2
parinya-siri <parinya.sirikatitum@gmail.com>
2022-08-05 20:03:41 +0100

    impliment model saver

--- neuralnetwork/.ipynb_checkpoints/fcn-checkpoint.py
+++ neuralnetwork/.ipynb_checkpoints/fcn-checkpoint.py
@@ -1,17 +1,15 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-############### set hyper parameters ###########
-num_lines_predict = 10 
-################################################
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, num_lines_predict, layer1, layer2, layer3):
         super().__init__()
-        self.fc1 = nn.Linear(102*num_lines_predict, 2048)
-        self.fc2 = nn.Linear(2048, 512)
-        self.fc3 = nn.Linear(512, 256)
-        self.fc4 = nn.Linear(256, 102)
+        self.num_lines_predict = 0 
+        self.fc1 = nn.Linear(102*num_lines_predict, layer1)
+        self.fc2 = nn.Linear(layer1, layer2)
+        self.fc3 = nn.Linear(layer2, layer3)
+        self.fc4 = nn.Linear(layer3, 102)
     
     def forward(self, x):
         x = F.relu(self.fc1(x))
--- train_fcn1file.py
+++ train_fcn1file.py
@@ -8,12 +8,15 @@ import torch.nn.functional as F
 from datetime import datetime
 from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
-writer = SummaryWriter()
+import sys
+
 ############ Set the Hyper parameters ###########
 
 EPOCHS = 200
 max_char = 102
-num_lines_predict = fcn.num_lines_predict
+num_lines_predict = 10
+layers = [int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3])]
+
 ############ Set the Paths ######################
 
 data_path = './matlib'
@@ -28,12 +31,15 @@ for path in p:
         util.preprocess(filename, preprocess_path, data_path)
     
 #################################################
-net = fcn.Net()
+net = fcn.Net(num_lines_predict, layers[0], layers[1], layers[2])
+now = datetime.now()
+save_path = './runs/' + now.strftime("%m-%m-%Y") + '-' + str(layers[0]) + '-' + str(layers[1]) + '-' + str(layers[2])
+writer = SummaryWriter(save_path)
 
 optimizer = optim.Adam(net.parameters(), lr=0.001)
 loss=0
 loss_list = []
-
+print('training fcn ', layers)
 for epoch in range(EPOCHS):
     print('training epoch number ',epoch)
     text = util.list_lines("./preprocess/algebra-associated.txt")
@@ -54,17 +60,18 @@ for epoch in range(EPOCHS):
         optimizer.step()
     writer.add_scalar("Loss/train", loss, epoch)
     writer.flush()  
+    torch.save(net, save_path + '/epoch-'+ epoch + '.pth')
     print('loss for ',epoch,' epochs : ',loss)
-    loss_list.append(loss.item())
+    # loss_list.append(loss.item())
   
     
 ############ Save ###################
-now = datetime.now()
-k=0
-while os.path.exists('./neuralnetwork/'+now.strftime("%m-%m-%Y")+'_'+str(k)+".txt"):
-    k+=1
-ppp = './neuralnetwork/'+'fcn'+now.strftime("%m-%m-%Y")+'_'+str(k)
-with open(ppp+".txt", "w") as f:
-    for s in loss_list:
-        f.write(str(s) +"\n")
-torch.save(net,ppp+'.pth')
+# now = datetime.now()
+# k=0
+# while os.path.exists('./neuralnetwork/'+now.strftime("%m-%m-%Y")+'_'+str(k)+".txt"):
+#     k+=1
+# ppp = './neuralnetwork/'+'fcn'+now.strftime("%m-%m-%Y")+'_'+str(k)
+# with open(ppp+".txt", "w") as f:
+#     for s in loss_list:
+#         f.write(str(s) +"\n")
+# torch.save(net,ppp+'.pth')
